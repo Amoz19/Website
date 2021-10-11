dda38b7cdbc542c76a1239d9aac8fb1521eaf32d
amoz18 <aungmon10co@gmail.com>
2021-10-11 15:04:48 +0630

    1110

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
-window.onscroll = () => {
-    let navBar = document.getElementById("navbar");
-    if (window.pageYOffset > 0) {
-      navBar.classList.add("fix");
-    } else {
-      navBar.classList.remove("fix");
-    }
-  };
+// window.onscroll = () => {
+//     let navBar = document.getElementById("navbar");
+//     if (window.pageYOffset > 0) {
+//       navBar.classList.add("fix");
+//     } else {
+//       navBar.classList.remove("fix");
+//     }
+//   };
