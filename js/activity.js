0ea32aa6ebe8fdc07cf6914b1aff8d60dbed7120
amoz18 <aungmon10co@gmail.com>
2021-10-17 15:15:26 +0630

    17102021

---
+++ js/activity.js
@@ -0,0 +1,20 @@
+function chng1() {
+    document.getElementById('img1').style.display="block"; 
+     document.getElementById('img2').style.display="none"; 
+     document.getElementById('img3').style.display="none"; 
+}
+function chng2() {
+    document.getElementById('img1').style.display="none"; 
+     document.getElementById('img2').style.display="block"; 
+     document.getElementById('img3').style.display="none"; 
+}
+function chng3() {
+    document.getElementById('img1').style.display="none"; 
+     document.getElementById('img2').style.display="none"; 
+     document.getElementById('img3').style.display="block"; 
+}
+function chng() {
+    document.getElementById('img1').style.display="block"; 
+     document.getElementById('img2').style.display="block"; 
+     document.getElementById('img3').style.display="block"; 
+}
